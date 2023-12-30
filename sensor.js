3e05b3c09aec958e848e385cd5ea7a822ea1bed2
Prasthuth <myshoppingapp.prasthuth@gmail.com>
2023-12-30 17:59:18 +0530

    Create sensor v3
    Updated the code to get ray line while having rayCount 1.

--- sensor.js
+++ sensor.js
@@ -1,7 +1,7 @@
 class Sensor {
     constructor(car) {
         this.car = car;
-        this.rayCount = 3;
+        this.rayCount = 1;
         this.rayLength = 100;
         this.raySpread = Math.PI / 4; // 45 degree
 
@@ -14,7 +14,7 @@ class Sensor {
             const rayAngle = lerp(
                 this.raySpread / 2,
                 -this.raySpread / 2,
-                i / (this.rayCount - 1)
+                this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
             ) + this.car.angle;
 
             const start = { x: this.car.x, y: this.car.y };
